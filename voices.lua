6d39a2720a27effd99a4afd3af30e43761fd9c8f
TechForVI <sabirrana112211@gmail.com>
2026-08-24 09:08:04 +0500

    Create voices.lua via GitHub by Tech For VI

---
+++ voices.lua
@@ -0,0 +1,23 @@
+voices = {
+    {"Nicole", "piTKgcLEGmPE4e6mEKli"},
+    {"Adam", "pNInz6obpgDQGcFmaJgB"},
+    {"Domi", "AZnzlk1XvdvUeBnXmlld"},
+    {"Bella", "EXAVITQu4vr4xnSDxMaL"},
+    {"Antoni", "ErXwobaYiN019PkySvjV"},
+    {"Elli", "MF3mGyEYCl7XYWbV9V6O"},
+    {"Josh", "TxGEqnHWrfWFTfGW9XjX"},
+    {"Rachel", "21m00Tcm4TlvDq8ikWAM"},
+    {"Samira", "C0lZz0a3b7KzGacx-2Jf"},
+    {"Laura", "zzFZp3dVi8pfnnF7QGJi"},
+    {"James", "2XFnWy_JjyM9y6aFtGfT"},
+    {"Oliver", "xrbK98Xl8XBdVE9N1mOg"},
+    {"Mia", "TwJhzpW7vNcfIs8gfQ9f"},
+    {"Liza", "w9z8gRoaP0knhV1WLdDo"},
+    {"Gabriel", "k1eR2wJXsT1qYAhG74Rg"},
+    {"Sofia", "zV0bxVkIqYk0YmQF_Z8t"},
+    {"Eli", "B8Mi1oSHJPRXZfC2fB2A"},
+    {"Megan", "v1He4h63g2zBwlEYwNw9"},
+    {"Hugo", "fd7M0HTjk1jzYwsrOLkX"}
+}
+
+return voices
